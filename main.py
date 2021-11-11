d28ec8207e025ef857013f557d8cc3f742928b61
Guoao Wei <guoao.wei@outlook.com>
2021-11-11 10:18:06 -0800

    Use doc.vector instead, rename vector file to vectors.npy

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import json, os
 import spacy
+from tqdm import tqdm
 
 
 nlp = spacy.load('en_core_web_md')
@@ -18,19 +19,25 @@ def load_emojis():
 emojis = load_emojis()
 df = pd.DataFrame(emojis, columns=['name', 'unicode', 'keywords', 'definition'])
 print(df['unicode'].head())
+
 sentencevector = []
 count = 0
-for i in range(len(emojis)):
+
+for i in tqdm(range(len(emojis))):
     desc = df['definition'][i] + df['name'][i]
-    token = nlp(desc)
-    vecs = [t.vector for t in token]
-    print(desc)
-    mean_vec = np.average(vecs)
-    sentencevector.append(mean_vec)
+    doc = nlp(desc)
+    # vecs = [t.vector for t in tokens]
+    # print(desc)
+    # mean_vec = np.mean(vecs, axis=0)
+    sentencevector.append(doc.vector)
     count=count+1
-print(len(sentencevector), count)
-print(sentencevector)
-np.save("vector.npy", sentencevector)
+
+sentencevector = np.asarray(sentencevector)
+
+assert len(sentencevector) == count
+print(sentencevector.shape)
+
+np.save("vectors.npy", sentencevector)
 # print(df['definition'])
 file = open('vocab.txt', 'w')
 for i in df['unicode']:
