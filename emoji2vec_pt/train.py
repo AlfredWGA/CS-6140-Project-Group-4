f41b323c6dc4484bfcbde400ce692f5b2141111e
Guoao Wei <guoao.wei@outlook.com>
2021-12-12 11:34:38 -0800

    Change the word2vec model path to pre-trained folder in the training script

--- emoji2vec_pt/train.py
+++ emoji2vec_pt/train.py
@@ -8,6 +8,7 @@ import pathlib
 import spacy
 import gensim
 from pytorch_lightning.loggers import WandbLogger
+import pandas as pd
 
 from dataset import EmojiDataset, load_emoji2vec_dataset, load_vocab
 from model import Emoji2Vec
@@ -21,12 +22,16 @@ vocab = load_vocab(current_path / "data" / "vocab.txt")
 train_df = load_emoji2vec_dataset(project_path / "emoji2vec" / "data" / "training" / "train.txt")
 train_neg_df = load_emoji2vec_dataset(project_path / "emoji2vec" / "data" / "raw_training_data" / "shuf_neg_samples.txt")
 
+train_desc_df = load_emoji2vec_dataset(project_path / "emoji2vec_pt" / "data" / "train_desc.txt")
+train_kw_df = load_emoji2vec_dataset(project_path / "emoji2vec_pt" / "data" / "train_keyword.txt")
+train_df = pd.concat([train_df, train_desc_df, train_kw_df])
+
 dev_df = load_emoji2vec_dataset(project_path / "emoji2vec" / "data" / "training" / "dev.txt")
 test_df = load_emoji2vec_dataset(project_path / "emoji2vec" / "data" / "training" / "test.txt")
 
 print("Loading SpaCy model and pre-trained Word2Vec model...")
 nlp = spacy.load("en_core_web_sm")
-word2vec = gensim.models.KeyedVectors.load_word2vec_format(project_path / 'GoogleNews-vectors-negative300.bin', binary=True)
+word2vec = gensim.models.KeyedVectors.load_word2vec_format(project_path / 'pre-trained' / 'GoogleNews-vectors-negative300.bin', binary=True)
 print("Loading complete.")
 
 embedding_dim = word2vec["hello"].shape[0]
@@ -45,9 +50,9 @@ val_dataloader = DataLoader(val_dataset, batch_size=batch_size)
 
 model = Emoji2Vec(num_embeddings=len(vocab), embedding_dim=embedding_dim, encoder_type="bow")
 
-wandb_logger = WandbLogger(project="emoji2vec", save_dir=current_path / "ckpt")
+wandb_logger = WandbLogger(project="emoji2vec_desc_kw", save_dir=current_path / "ckpt")
 
-trainer = pl.Trainer(callbacks=[EarlyStopping(monitor="val_loss")],
+trainer = pl.Trainer(callbacks=[EarlyStopping(monitor="val_loss", min_delta=0.001)],
                     gpus=1, # Remove this if training with cpus
                     logger=wandb_logger
                     )
